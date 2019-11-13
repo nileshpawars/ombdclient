dbb08d0a6f00bc9ecb7dca63877b989b82e6966a
Pawar, Nilesh (BOM-CWW) <Nilesh.Pawar@craftww.com>
2019-11-13 18:49:54 +0530

    Updates

---
+++ precache-manifest.4629d133e0d9d0cde467c98e4f28e878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b79159f34403d1006e1a3eded17e6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60b2df99b2348c8d40ec",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "711f4d2615bc359c1539",
+    "url": "/static/css/main.9aed391f.chunk.css"
+  },
+  {
+    "revision": "60b2df99b2348c8d40ec",
+    "url": "/static/js/2.b933fdd5.chunk.js"
+  },
+  {
+    "revision": "711f4d2615bc359c1539",
+    "url": "/static/js/main.3782b708.chunk.js"
+  },
+  {
+    "revision": "b999aba669008ae737a5",
+    "url": "/static/js/runtime-main.35d94f8b.js"
+  }
+]);
